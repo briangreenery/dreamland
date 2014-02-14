31c9ee4df90520fe804072ea6738a8ebd366966a
Brian Green <briangreenery@gmail.com>
2014-02-14 14:55:27 -0800

    Try to build zlib also

--- Applications/Console/main.cpp
+++ Applications/Console/main.cpp
@@ -1,5 +1,7 @@
 #include "Core/Integers.h"
 #include "Core/Hodor.h"
+
+#include <zlib.h>
 #include <iostream>
 
 int main()
@@ -7,5 +9,10 @@ int main()
 	uint32 i = 0;
 	std::cout << "The value of i is: " << i << std::endl;
 	std::cout << "The value of Hodor is: " << Hodor() << std::endl;
+
+	z_stream stream;
+	inflateInit( &stream );
+	std::cout << "Initalized zstream" << std::endl;
+
 	return 0;
 }
